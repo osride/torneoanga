ef95f6a988662431ce5c6cfa0b04be6f5a832cba
Osvaldo Gabilán <osvaldo.gabilan@zeke.cl>
2025-06-30 21:52:08 -0400

    Segundo intento

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // MUY IMPORTANTE para rutas relativas
   server: {
     port: 3000
   }
